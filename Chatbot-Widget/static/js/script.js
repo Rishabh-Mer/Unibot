7586bec1f5c27c44e613f9ca0fd8190c0351205b
Anand <anand.agrawal84@gmail.com>
2020-12-09 12:34:07 +0530

    correct user avatar url in JS

--- Chatbot-Widget/static/js/script.js
+++ Chatbot-Widget/static/js/script.js
@@ -126,7 +126,7 @@ $("#sendButton").on("click", function (e) {
 
 //==================================== Set user response =====================================
 function setUserResponse(message) {
-	var UserResponse = '<img class="userAvatar" src=' + "./static/img/userAvatar.jpg" + '><p class="userMsg">' + message + ' </p><div class="clearfix"></div>';
+	var UserResponse = '<img class="userAvatar" src="' + new URL(window._env_.jsonServerUrl).origin + '/static/img/userAvatar.jpg"' + '><p class="userMsg">' + message + ' </p><div class="clearfix"></div>';
 	$(UserResponse).appendTo(".chats").show("slow");
 
 	$(".usrInput").val("");
