8aa9c01aa79a483f37e1e192812d087e0684d4f6
Anand Agrawal <anand.agrawal84@gmail.com>
2021-08-16 18:15:21 +0530

    update sms template

--- sms_service.py
+++ sms_service.py
@@ -1,5 +1,5 @@
 import requests
 
 def SendOtp(mobileNumber, otp):
-    url = 'https://apps.sandeshlive.com/API/WebSMS/Http/v1.0a/index.php?userid=750&password=3Kq5m98PHAafxQ0N&sender=UniGrp&to={0}&message=OTP is {1}&reqid=1&format=text'.format(mobileNumber, otp)
+    url = 'https://apps.sandeshlive.com/API/WebSMS/Http/v1.0a/index.php?userid=1190&password=YaGpo7LdXOt5wiDA&sender=UNIVSE&to={0}&message=Dear Student/Parent, OTP is {1} - UNIVSE&reqid=1&format=text'.format(mobileNumber, otp)
     requests.get(url)
